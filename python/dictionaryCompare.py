6f3d386713c887e24a51b8cc0a38d370cc940b5f
lemonadern <taishi.develop@gmail.com>
2021-08-20 06:49:47 +0900

    comparing objects

---
+++ python/dictionaryCompare.py
@@ -0,0 +1,15 @@
+obj = {
+    "key1": "KEY1",
+    "ob": {
+        "o": "nested property"
+    }
+}
+
+anotherObj = {
+    "key1": "KEY1",
+    "ob": {
+        "o": "nested property"
+    }
+}
+
+print(obj == anotherObj)
